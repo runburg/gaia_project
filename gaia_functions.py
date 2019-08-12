04381afae1781358025964e660e0acfd5b090b3a
Jack Runburg <jack.runburg@gmail.com>
2019-08-11 14:36:49 -1000

    Working colorbars for all plots

--- gaia_functions.py
+++ gaia_functions.py
@@ -140,156 +140,34 @@ def trim_axes(axes, N):
         return [axes]
 
 
-def plot_setup():
+def plot_setup(rows, cols, d=0, buffer=(0, 0)):
     """Set mpl parameters for beautification."""
-    return None
-
-
-def parallax_histograms(input_file, input_pm_file, output_file, titles=None, cut=100):
-    """Generate the temperature maps for pms of input objects."""
-    # get titles for each subplot and dwarf proper motions
-    dwarf_specs = ascii.read(input_file, format='ecsv')
-    if titles is None:
-        titles = [f'RA: {np.round(ra, 2)}, DEC: {np.round(dec, 2)}' for (ra, dec) in zip(dwarf_specs['RA'], dwarf_specs['DEC'])]
-        dwarf_pmra, dwarf_pmdec = None, None
-    else:
-        titles = dwarf_specs['MAIN_ID']
-        dwarf_pmra = dwarf_specs['PMRA']
-        dwarf_pmdec = dwarf_specs['PMDEC']
-
-    # load stellar pm values
-    with np.load(input_pm_file, 'rb', allow_pickle=True) as infile:
-        # vels = infile['vels']
-        pmra = infile['pmra']
-        pmdec = infile['pmdec']
-        parallax = infile['parallax']
-        ra = infile['ra']
-        dec = infile['dec']
-
     # setup plot
     plt.close('all')
-    mpl.rcParams['axes.labelsize'] = 'x-large'
+    mpl.rcParams['axes.labelsize'] = 'large'
     mpl.rcParams['ytick.labelsize'] = 'x-small'
     mpl.rcParams['xtick.labelsize'] = 'x-small'
+    mpl.rcParams['figure.subplot.wspace'] = buffer[0]
+    mpl.rcParams['figure.subplot.hspace'] = buffer[1]
 
-    # set fig size and shape
-    rows = 3
-    cols = 3
-    figsize = (3 * cols, 2 + 2 * rows)
-    fig, ax = plt.subplots(nrows=rows, ncols=cols, figsize=figsize)
-
-    # plot each dwarf in separate subplots
-    for i in range(0, rows):
-        # set up symmetrical bins about axis
-        bins = np.logspace(-2, 0, num=30)
-
-        # add quiver plot
-        cut_data = cut_on_parallax(ra[i], dec[i], pmra[i], pmdec[i], cut=cut)
-        pm_mag = np.hypot(cut_data[2], cut_data[3])
-        arrows = ax[i, 0].quiver(*cut_data, color=cm.gnuplot(pm_mag), clim=(0, pm_mag.max()), units='x', pivot='tail', width=0.005)
-        colorbar_for_subplot(fig, ax[i, 0], arrows)
-        ax[i, 0].set_facecolor('xkcd:black')
-        ax[i, 0].set_title(titles[i].strip('NAME ')+' quiver')
-
-        # generate histogram for each cone
-        n, bins, patches = ax[i, 1].hist(parallax[i], bins=bins)
-        ax[i, 1].set_title(titles[i].strip('NAME ')+' parallax histogram')
-        ax[i, 1].set_xscale('log')
-
-        # add pm 2d histograms
-        bins = np.linspace(-15, 15, num=180)
-        counts, xedges, yedges, im = ax[i, 2].hist2d(pmra[i], pmdec[i], bins=(bins, bins), vmin=0, cmap='gnuplot')
-        if dwarf_pmra is not None:
-            ax[i, 2].plot(dwarf_pmra[i], dwarf_pmdec[i], marker='x', color='xkcd:cyan', alpha=0.5)
-
-        ax[i, 2].set_title(titles[i].strip('NAME '))
-        colorbar_for_subplot(fig, ax[i, 2], im)
-
-    plt.tight_layout()
-
-    # add a universal colorbar, change cmap in hist2d above
-    # fig.colorbar(im, ax=axs.ravel().tolist())
-
-    fig.savefig(output_file, bbox_inches='tight')
-
-
-def make_pm_maps(input_file, input_pm_file, output_file, num_cones, num_bins=80, titles=None, mincount=0, maxcount=40):
-    """Generate the temperature maps for pms of input objects."""
-    # get titles for each subplot and dwarf proper motions
-    dwarf_specs = ascii.read(input_file, format='ecsv')
-    print(dwarf_specs['RA'])
-    print(dwarf_specs['DEC'])
-    if titles is None:
-        titles = [str('RA: ' + str(np.round(ra, 2)) + ', DEC: ' + str(np.round(dec, 2))) for (ra, dec) in zip(dwarf_specs['RA'], dwarf_specs['DEC'])]
-        dwarf_pmra, dwarf_pmdec = None, None
-    else:
-        titles = dwarf_specs['MAIN_ID']
-        dwarf_pmra = dwarf_specs['PMRA']
-        dwarf_pmdec = dwarf_specs['PMDEC']
-
-    # load stellar pm values
-    with np.load(input_pm_file, 'rb', allow_pickle=True) as infile:
-        # vels = infile['vels']
-        pmra = infile['pmra']
-        pmdec = infile['pmdec']
-
-    # setup plot
-    plt.close('all')
-    mpl.rcParams['axes.labelsize'] = 'x-large'
-    mpl.rcParams['ytick.labelsize'] = 'x-small'
-    mpl.rcParams['xtick.labelsize'] = 'x-small'
-
-    # set fig size and shape
-    d = len(titles)
-    rows = 3
-    cols = int(np.ceil(d/rows))
-    figsize = (4 + 3 * cols, 2 + 2 * rows)
+    figsize = (6 * cols + buffer[0], 5.5 * rows + buffer[1])
     fig, axs = plt.subplots(nrows=rows, ncols=cols, figsize=figsize)
-    axs = trim_axes(axs, d)
-    max_count = [0, 0]
+    if d is not 0:
+        axs = trim_axes(axs, d)
 
-    # plot each dwarf in separate subplots
-    for i, ax in enumerate(fig.axes):
-        # set up symmetrical bins about axis
-        # bins = np.logspace(-2, 2, num=num_bins//2)
-        # bins = np.concatenate((np.flip(np.negative(bins)), bins))
-        bins = np.linspace(-15, 15, num=180)
-
-        # generate temp map for each cone
-        counts, xedges, yedges, im = ax.hist2d(pmra[i], pmdec[i], bins=(bins, bins), vmin=0, cmap='gnuplot')
-        if counts.max() > max_count[1]:
-            max_count = [i, counts.max()]
-
-        # plot the proper motion of each dwarf from simbad
-        if dwarf_pmra is not None:
-            ax.plot(dwarf_pmra[i], dwarf_pmdec[i], marker='x', color='xkcd:cyan', alpha=0.5)
-
-        ax.set_title(titles[i].strip('NAME '))
-        colorbar_for_subplot(fig, ax, im)
-        # ax.set_xscale('symlog', linthreshx=0.1)
-        # ax.set_yscale('symlog', linthreshy=0.1)
-
-    # make labels across all subplots
-    universal_plot_labels(fig, r"Proper motion, right ascension [mas/yr]", r"Proper motion, declination [mas/yr]")
-
-    plt.tight_layout()
-
-    # add a universal colorbar, change cmap in hist2d above
-    # fig.colorbar(im, ax=axs.ravel().tolist())
-
-    fig.savefig(output_file, bbox_inches='tight')
-    print('max count is: ' + str(max_count))
+    return fig, axs
 
 
-def cut_on_parallax(xcoord, ycoord, xpm, ypm, cut):
-    """Return objects with proper motion magnitude below cut."""
-    indices = [np.hypot(xpp, ypp) < cut for xpp, ypp in zip(xpm,ypm)]
-    xpm = xpm[indices]
-    ypm = ypm[indices]
-    xcoord = xcoord[indices]
-    ycoord = ycoord[indices]
+def colorbar_for_subplot(fig, axs, cmap, image):
+    """Place a colorbar by each plot."""
+    from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-    return xcoord, ycoord, xpm, ypm
+    divider = make_axes_locatable(axs)
+    cax = divider.append_axes('right', size='5%', pad=0.05)
+    cbar = fig.colorbar(image, cax=cax)
+    # cm.ScalarMappable(norm=None, cmap=cmap),
+
+    return cbar
 
 
 def universal_plot_labels(fig, xlabel, ylabel):
@@ -301,55 +179,234 @@ def universal_plot_labels(fig, xlabel, ylabel):
     plt.tight_layout()
 
 
-def colorbar_for_subplot(fig, axs, plot):
-    """Place a colorbar by each plot."""
-    from mpl_toolkits.axes_grid1 import make_axes_locatable
+def cut_on_parallax(ra, dec, pmra, pmdec, parallax, cut):
+    """Return objects with parallax magnitude below cut."""
+    indices = [par < cut for par in parallax]
+    pmra = pmra[indices]
+    pmdec = pmdec[indices]
+    ra = ra[indices]
+    dec = dec[indices]
+    parallax = parallax[indices]
 
-    divider = make_axes_locatable(axs)
-    cax = divider.append_axes('right', size='5%', pad=0.05)
-    fig.colorbar(plot, cax=cax)
+    return ra, dec, pmra, pmdec, parallax
 
 
-def pm_vector_plot(xcoord, ycoord, xpm, ypm, outfile, names=None, cut=10, cmap=cm.gnuplot):
-    """Make vector plots of pm data."""
-    d = len(xcoord)
-    rows = d//4
-    if rows==0:
-        rows=1
+def load_dwarf_info(file, titles=None):
+    """Load the dwarf info from Simbad files."""
+    dwarf_specs = ascii.read(file, format='ecsv')
+
+
+    if titles is None:
+        titles = [f'RA: {np.round(ra, 2)}, DEC: {np.round(dec, 2)}' for (ra, dec) in zip(dwarf_specs['RA'], dwarf_specs['DEC'])]
+        dwarf_pmra, dwarf_pmdec = [None]*len(titles), [None]*len(titles)
+    else:
+        titles = dwarf_specs['MAIN_ID']
+        dwarf_pmra = dwarf_specs['PMRA']
+        dwarf_pmdec = dwarf_specs['PMDEC']
+
+    return np.array(titles), dwarf_pmra, dwarf_pmdec
+
+
+def load_gaia_search_info(file):
+    """Return stellar parameters from gaia search."""
+    with np.load(file, 'rb', allow_pickle=True) as infile:
+        # vels = infile['vels']
+        pmra = infile['pmra']
+        pmdec = infile['pmdec']
+        parallax = infile['parallax']
+        ra = infile['ra']
+        dec = infile['dec']
+
+    return ra, dec, pmra, pmdec, parallax
+
+
+def parallax_histogram(fig, ax, data, title, cut=None):
+    """Create a histogram of parallax values."""
+    if cut is not None:
+        ra, dec, pmra, pmdec, parallax, = cut_on_parallax(*data, cut)
+    else:
+        ra, dec, pmra, pmdec, parallax, = data
+
+    # bins = np.logspace(-2, 0, num=30)
+    bins = np.linspace(parallax.min(), parallax.max(), num = 30)
+    n, bins, patches = ax.hist(parallax, bins=bins)
+    ax.set_title(title)
+    ax.set_xlabel('Parallax [mas]')
+    ax.set_ylabel('Bin counts')
+    # ax.set_xscale('log')
+
+    return n, bins, patches
+
+
+def quiver_plot(fig, ax, data, title, cut=None, colorbar=True, radius=None):
+    """Create quiver plot of stellar position and proper motion."""
+    if cut is not None:
+        ra, dec, pmra, pmdec, parallax, = cut_on_parallax(*data, cut)
+    else:
+        ra, dec, pmra, pmdec, parallax, = data
+
+    pm_mag = np.hypot(pmra, pmdec)
+    pmra = pmra / pm_mag
+    pmdec = pmdec / pm_mag
+
+    cmap = cm.viridis
+    arrows = ax.quiver(ra, dec, pmra, pmdec, color=cmap(pm_mag), clim=(0, pm_mag.max()), units='xy', pivot='tail', width=0.01, headwidth=2, headlength=3, minlength=0.01)
+    cbar = colorbar_for_subplot(fig, ax, cmap, image=arrows)
+    ax.set_facecolor('xkcd:black')
+
+    if radius is not None:
+        ax.set_title(title + f', density: {np.round(len(ra)/(np.pi * radius**2), 2)}')
+        with open('densities.txt', 'a') as outfile:
+            outfile.write(f'{title}\t {np.round(len(ra)/(np.pi * radius**2), 2)}\n')
+    else:
+        ax.set_title(title)
+
+    ax.set_xlabel(r"Right ascension [$^\circ$]")
+    ax.set_ylabel(r"Declination [$^\circ$]")
+    cbar.ax.set_ylabel("Proper motion magnitude [mas/yr]", rotation=270, labelpad=10)
+
+
+    return arrows
+
+
+def pm_histogram(fig, ax, data, title, dwarf_pmra=None, dwarf_pmdec=None, cut=None, colorbar=True):
+    """Create 2d histogram of proper motions from GAIA search."""
+    if cut is not None:
+        ra, dec, pmra, pmdec, parallax, = cut_on_parallax(*data, cut)
+    else:
+        ra, dec, pmra, pmdec, parallax, = data
+
+    # bin data from gaia in 2d histogram
+    bound = 5
+    bins = np.linspace(-bound, bound, num=20*bound)
+    counts, xedges, yedges, im = ax.hist2d(pmra, pmdec, bins=(bins, bins), vmin=0, cmap='gnuplot')
+
+    # plot pm motion of dwarf from simbad
+    if dwarf_pmra is not None:
+        ax.plot(dwarf_pmra, dwarf_pmdec, marker='x', markersize=15, color='xkcd:cyan', alpha=0.5)
+
+    ax.set_title(title)
+    ax.set_xlabel(r"Right ascension proper motion [mas/yr])")
+    ax.set_ylabel(r"Declination proper motion [mas/yr]")
+
+    cbar = colorbar_for_subplot(fig, ax, cm.gnuplot, image=im)
+    cbar.ax.set_ylabel("Bin counts", rotation=270, labelpad=10)
+
+    return counts, xedges, yedges, im
+
+
+def draco_comparison_plot(input_file, input_pm_file, output_file, cuts, dwarf=0, titles=None, radius=0):
+    """Generate the temperature maps for pms of input objects."""
+
+    # get titles for each subplot and dwarf proper motions
+    titles, dwarf_pmra, dwarf_pmdec = load_dwarf_info(input_file, titles)
+
+    # load stellar pm values
+    ra, dec, pmra, pmdec, parallax = load_gaia_search_info(input_pm_file)
+
+    # set fig size and shape
+    fig, axs = plot_setup(len(cuts), 3)
+
+    # plot each dwarf in separate subplots
+    for j, (title, *data) in enumerate(zip(titles, ra, dec, pmra, pmdec, parallax)):
+        if j==dwarf:
+            for i, cut in enumerate(cuts):
+                # add quiver plot
+                arrows = quiver_plot(fig, axs[i, 0], data, f"{title.strip('NAME ')} quiver", cut=cut, radius=radius)
+
+                # generate parallax histogram
+                n, bins, patches = parallax_histogram(fig, axs[i, 1], data, f"{title.strip('NAME ')} parallax, cut={cut} mas", cut=cut)
+
+                # add pm 2d histograms
+                counts, xedges, yedges, im = pm_histogram(fig, axs[i, 2], data, f"{title.strip('NAME ')} pm histo", dwarf_pmra=dwarf_pmra[j], dwarf_pmdec=dwarf_pmdec[j], cut=cut)
+
+    plt.tight_layout()
+
+
+    fig.savefig(output_file, bbox_inches='tight')
+
+
+def comparison_plot(input_file, input_pm_file, output_file, titles=None, cut=None):
+    """Generate the temperature maps for pms of input objects."""
+
+    # get titles for each subplot and dwarf proper motions
+    titles, dwarf_pmra, dwarf_pmdec = load_dwarf_info(input_file, titles)
+
+    # load stellar pm values
+    ra, dec, pmra, pmdec, parallax = load_gaia_search_info(input_pm_file)
+
+    # set fig size and shape
+    fig, axs = plot_setup(3, 3)
+
+    # choose how many dwarfs to compare
+    counter = range(0, 3)
+
+    # plot each dwarf in separate subplots
+    for (i, title, *data) in zip(counter, titles, ra, dec, pmra, pmdec, parallax):
+        # add quiver plot
+        arrows = quiver_plot(fig, axs[i, 0], data, f"{title.strip('NAME ')} quiver", cut=cut)
+
+        # generate parallax histogram
+        n, bins, patches = parallax_histogram(fig, axs[i, 1], data, f"{title.strip('NAME ')} parallax", cut=cut)
+
+        # add pm 2d histograms
+        counts, xedges, yedges, im = pm_histogram(fig, axs[i, 2], data, f"{title.strip('NAME ')} pm histo", dwarf_pmra=dwarf_pmra[i], dwarf_pmdec=dwarf_pmdec[i], cut=cut)
+
+    plt.tight_layout()
+
+
+    fig.savefig(output_file, bbox_inches='tight')
+
+
+def make_pm_maps(input_file, input_pm_file, output_file, num_cones, num_bins=80, titles=None, mincount=0, maxcount=40, cut=None):
+    """Generate the temperature maps for pms of input objects."""
+    # get titles for each subplot and dwarf proper motions
+    titles, dwarf_pmra, dwarf_pmdec, = load_dwarf_info(input_file, titles)
+
+    # load stellar pm values
+    ra, dec, pmra, pmdec, parallax, = load_gaia_search_info(input_pm_file)
+
+    # set fig size and shape
+    d = len(titles)
+    rows = 3
     cols = int(np.ceil(d/rows))
-    figsize = (3.5 * cols, 2.75 * rows)
-    fig, axs = plt.subplots(nrows=rows, ncols=cols, figsize=figsize)
-    # fig.tight_layout()
-    axs = trim_axes(axs, d)
+    fig, axs = plot_setup(rows, cols, d)
+    max_count = [0, 0]
 
-    for i, (ax, name) in enumerate(zip(axs, names)):
-        cut_data = cut_on_parallax(xcoord[i], ycoord[i], xpm[i], ypm[i], cut=cut)
-        pm_mag = np.hypot(cut_data[2], cut_data[3])
+    # plot each dwarf in separate subplots
+    for ax, title, dwarfpmra, dwarfpmdec, *data in zip(axs, titles, dwarf_pmra, dwarf_pmdec, ra, dec, pmra, pmdec, parallax):
+        counts, xedges, yedges, im = pm_histogram(fig, ax, data, f"{title.strip('NAME ')}", dwarf_pmra=dwarfpmra, dwarf_pmdec=dwarfpmdec, cut=cut)
 
-        arrows = ax.quiver(*cut_data, color=cmap(pm_mag), clim=(0, pm_mag.max()), units='x', pivot='tail', width=0.005)
-        # qk = ax3.quiverkey(Q, 0.9, 0.9, 1, r'$1 \frac{m}{s}$', labelpos='E', coordinates='figure')
+    # make labels across all subplots
+    universal_plot_labels(fig, r"Proper motion, right ascension [mas/yr]", r"Proper motion, declination [mas/yr]")
 
-        colorbar_for_subplot(fig, ax, arrows)
-        ax.set_facecolor('xkcd:black')
-        ax.set_title(name.strip('NAME '))
+    # add a universal colorbar, change cmap in hist2d above
+    # fig.colorbar(im, ax=axs.ravel().tolist())
+
+    fig.savefig(output_file, bbox_inches='tight')
+
+
+def pm_vector_plot(input_file, input_pm_file, outfile, titles=None, cut=None, cmap=cm.gnuplot, radius=None):
+    """Make vector plots of pm data."""
+    # load dwarf info from simbad
+    titles, dwarf_pmra, dwarf_pmdec, = load_dwarf_info(input_file, titles)
+
+    # load pm values
+    ra, dec, pmra, pmdec, parallax, = load_gaia_search_info(input_pm_file)
+
+    # set parameters for subplots
+    d = len(ra)
+    rows = int(np.ceil(d/4))
+    cols = int(np.ceil(d/rows))
+    fig, axs = plot_setup(rows, cols, d)
+
+    for i, (ax, title, *data) in enumerate(zip(axs, titles, ra, dec, pmra, pmdec, parallax)):
+        arrows = quiver_plot(fig, ax, data, title.strip("NAME "), cut=cut, radius=radius)
 
     universal_plot_labels(fig, r'Right Ascension [$^\circ$]', r'Declination [$^\circ$]')
     fig.savefig(outfile, bbox_inches='tight')
 
 
 if __name__ == '__main__':
-    dwarf_specs = ascii.read('./gaia_data/dwarf_info.ecsv', format='ecsv')
-    xcoords = dwarf_specs['RA']
-    ycoords = dwarf_specs['DEC']
-    titles = dwarf_specs['MAIN_ID']
-
-    # load pm values
-    with np.load('./gaia_data/dwarf_vels.npz', allow_pickle=True) as infile:
-        # vels = infile['vels']
-        pmra = infile['pmra']
-        pmdec = infile['pmdec']
-        ra = infile['ra']
-        dec = infile['dec']
-
-    pm_vector_plot(ra, dec, pmra, pmdec, './plots/dwarf_quivers.pdf', names=titles)
-    plt.show()
+    for radius in [0.5, 0.1, 0.05]:
+        pm_vector_plot(f'./gaia_data/randomcone_info_{radius}.ecsv', f'./gaia_data/randomcone_vels_{radius}.npz', f'./plots/random_quivers_{radius}.pdf', titles=None, radius=radius)
--- gaia_plotting.py
+++ gaia_plotting.py
@@ -12,8 +12,19 @@ import matplotlib.pyplot as plt
 import glob
 
 radii = [0.5, 0.1, 0.05]
+dwarf = 0
+dwarf_dict = {
+    0: 'draco',
+    1: 'leoI',
+    2: 'leoB',
+    3: 'umi',
+    4: 'sculptor'
+    }
+# radii = [0.1]
 for radius in radii:
-    # g.make_pm_maps(f'./gaia_data/dwarf_info_{radius}.ecsv', f'./gaia_data/dwarf_vels_{radius}.npz', f'./plots/known_dwarf_histos_{radius}.pdf', num_cones=8, titles=1)
-    # g.make_pm_maps(f'./gaia_data/randomcone_info_{radius}.ecsv', f'./gaia_data/randomcone_vels_{radius}.npz', f'./plots/random_cone_histos_{radius}.pdf', num_cones=8)
-    g.parallax_histograms(f'./gaia_data/dwarf_info_{radius}.ecsv', f'./gaia_data/dwarf_vels_{radius}.npz', f'./plots/known_dwarf_parallax_histos_{radius}.pdf', titles=1)
-    g.parallax_histograms(f'./gaia_data/randomcone_info_{radius}.ecsv', f'./gaia_data/randomcone_vels_{radius}.npz', f'./plots/random_cone_parallax_histos_{radius}.pdf')
+    g.make_pm_maps(f'./gaia_data/dwarf_info_{radius}.ecsv', f'./gaia_data/dwarf_vels_{radius}.npz', f'./plots/known_dwarf_histos_{radius}.pdf', num_cones=8, titles=1)
+    g.make_pm_maps(f'./gaia_data/randomcone_info_{radius}.ecsv', f'./gaia_data/randomcone_vels_{radius}.npz', f'./plots/random_cone_histos_{radius}.pdf', num_cones=8)
+    g.comparison_plot(f'./gaia_data/dwarf_info_{radius}.ecsv', f'./gaia_data/dwarf_vels_{radius}.npz', f'./plots/known_dwarf_parallax_histos_{radius}.pdf', titles=1)
+    g.comparison_plot(f'./gaia_data/randomcone_info_{radius}.ecsv', f'./gaia_data/randomcone_vels_{radius}.npz', f'./plots/random_cone_parallax_histos_{radius}.pdf')
+    g.draco_comparison_plot(f'./gaia_data/dwarf_info_{radius}.ecsv', f'./gaia_data/dwarf_vels_{radius}.npz', f'./plots/{dwarf_dict[dwarf]}_comparison_plot_{radius}.pdf', cuts=[0.05, 0.1, 0.5, 1], dwarf=dwarf, titles=1, radius=radius)
+    g.draco_comparison_plot(f'./gaia_data/randomcone_info_{radius}.ecsv', f'./gaia_data/randomcone_vels_{radius}.npz', f'./plots/random{dwarf+1}_comparison_plot_{radius}.pdf', cuts=[0.05, 0.1, 0.5, 1], dwarf=dwarf, radius=radius)
